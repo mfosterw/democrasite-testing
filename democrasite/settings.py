7c354f9c68c3dcd955de6aeaba487a7003e7199c
Matthew Foster Walsh <mfosterwalsh@mail.com>
2021-04-26 01:28:28 -0400

    Removed outdated comments from settings

--- democrasite/settings.py
+++ democrasite/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import environ
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-
 root = environ.Path(__file__) - 2
 env = environ.Env()
 try:
@@ -25,8 +23,6 @@ except FileNotFoundError:
 SITE_ROOT = root()
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str('SECRET_KEY')
